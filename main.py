627518e17836c9200a2e4cdfd8937a17d7b280d0
NathanGengeKeyin <109616859+NathanGengeKeyin@users.noreply.github.com>
2022-08-17 10:20:58 -0230

    Added FPS Counter
    added fps counter so my computer dont catch on fire

--- main.py
+++ main.py
@@ -3,18 +3,23 @@ import pygame
 
 pygame.font.init()
 
+# STATIC VARIABLES #
 WIDTH, HEIGHT = 800, 800
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 BOARD = pygame.image.load(os.path.join('Assets', 'Board.png'))
+FPS = 10
+####################
 
 def draw_window():
     WINDOW.blit(BOARD, (0, 0))
     pygame.display.update()
 
 def main():
-
+    
+    clock = pygame.time.Clock()
     run = True
     while run:
+        clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
@@ -27,3 +32,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+    
