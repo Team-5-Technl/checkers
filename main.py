7ca7b3c0287205546b4447a5d99d0a1fd278180b
NathanGengeKeyin <109616859+NathanGengeKeyin@users.noreply.github.com>
2022-08-17 10:01:00 -0230

    Initial game window
    started the game window and draw the board on the window

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
+import os
+import pygame
 
+pygame.font.init()
+
+WIDTH, HEIGHT = 800, 800
+WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+BOARD = pygame.image.load(os.path.join('Assets', 'Board.png'))
+
+def draw_window():
+    WINDOW.blit(BOARD, (0, 0))
+    pygame.display.update()
+
+def main():
+
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.quit()
+
+        draw_window()
+
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
