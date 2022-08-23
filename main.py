b6f2e07c16b14ae47b98b21251783ca3815c0019
lakshmichedella <lchedella_s@nlesd.ca>
2022-08-23 09:54:32 -0230

    Update main.py
    Capturing finished

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ BOARD = pygame.image.load(os.path.join('Assets', 'Board.png'))
 FPS = 10
 black_color = (88, 16, 0)
 selected_black_color = (51, 41, 39)
+white_score = 0
+black_score = 0
 ####################
 white_pieces = {}
 black_pieces = {}
@@ -46,7 +48,8 @@ def move_white_piece(x):
     pos = pygame.mouse.get_pos()
     rect = list(white_pieces.values())[list(white_pieces.values()).index(x)]
     if 50 < abs(pos[0]-rect.center[0]) < 150 and 50 < pos[1]-rect.center[1] < 150:
-        if (rect.x+([-100, 100][pos[0]-rect.x > 0]), rect.y+100, 50, 50) not in (white_pieces | black_pieces).values():
+        new_place = (rect.x + ([-100, 100][pos[0] - rect.x > 0]), rect.y + 100, 50, 50)
+        if new_place not in (white_pieces | black_pieces).values():
             '''
             This is for when we have kings
 if rect in kings.values():
@@ -58,6 +61,9 @@ if rect in kings.values():
             rect.x += ([-100, 100][pos[0]-rect.x > 0])
             rect.y += 100
             pygame.draw.ellipse(WINDOW, 'white', rect)
+            return True
+        if new_place not in white_pieces.values() and new_place in black_pieces.values():
+            capture_black_piece(x)
             return True
         else:
             print('Another piece at selected location. Try again')
@@ -71,11 +77,15 @@ def move_black_piece(x):
     pos = pygame.mouse.get_pos()
     rect = list(black_pieces.values())[list(black_pieces.values()).index(x)]
     if 50 < abs(pos[0]-rect.center[0]) < 150 and -150 < pos[1]-rect.center[1] < -50:
-        if (rect.x+([-100, 100][pos[0]-rect.x > 0]), rect.y-100, 50, 50) not in (white_pieces | black_pieces).values():
+        new_place = (rect.x+([-100, 100][pos[0]-rect.x > 0]), rect.y-100, 50, 50)
+        if new_place not in (white_pieces | black_pieces).values():
             pygame.draw.ellipse(WINDOW, 'black', rect)
             rect.x += ([-100, 100][pos[0]-rect.x > 0])
             rect.y -= 100
             pygame.draw.ellipse(WINDOW, black_color, rect)
+            return True
+        if new_place not in black_pieces.values() and new_place in white_pieces.values():
+            capture_white_piece(x)
             return True
         else:
             print('Another piece at selected location. Try again')
@@ -83,6 +93,46 @@ def move_black_piece(x):
     else:
         print('Illegal move. Try again')
         pygame.draw.ellipse(WINDOW, black_color, rect)
+
+
+# for black pieces
+def capture_white_piece(x):
+    global black_score
+    pos = pygame.mouse.get_pos()
+    rect = list(black_pieces.values())[list(black_pieces.values()).index(x)]
+    new_place = (rect.x + ([-100, 100][pos[0] - rect.x > 0]), rect.y - 100, 50, 50)
+    if (rect.x + ([-200, 200][pos[0] - rect.x > 0]), rect.y - 200, 50, 50) not in (white_pieces | black_pieces).values():
+        pygame.draw.ellipse(WINDOW, 'black', rect)
+        rect.x += ([-200, 200][pos[0] - rect.x > 0])
+        rect.y -= 200
+        white_capture = list(white_pieces.keys())[list(white_pieces.values()).index(new_place)]
+        pygame.draw.ellipse(WINDOW, black_color, rect)
+        pygame.draw.ellipse(WINDOW, 'black', white_pieces[white_capture])
+        del white_pieces[white_capture]
+        black_score += 1
+    else:
+        print('Capture failed. Another piece. Try again')
+        pygame.draw.ellipse(WINDOW, black_color, rect)
+
+
+# for white pieces
+def capture_black_piece(x):
+    global white_score
+    pos = pygame.mouse.get_pos()
+    rect = list(white_pieces.values())[list(white_pieces.values()).index(x)]
+    new_place = (rect.x + ([-100, 100][pos[0] - rect.x > 0]), rect.y + 100, 50, 50)
+    if (rect.x + ([-200, 200][pos[0] - rect.x > 0]), rect.y + 200, 50, 50) not in (white_pieces | black_pieces).values():
+        pygame.draw.ellipse(WINDOW, 'black', rect)
+        rect.x += ([-200, 200][pos[0] - rect.x > 0])
+        rect.y += 200
+        black_capture = list(black_pieces.keys())[list(black_pieces.values()).index(new_place)]
+        pygame.draw.ellipse(WINDOW, 'white', rect)
+        pygame.draw.ellipse(WINDOW, 'black', black_pieces[black_capture])
+        del black_pieces[black_capture]
+        white_score += 1
+    else:
+        print('Capture failed. Another piece. Try again')
+        pygame.draw.ellipse(WINDOW, 'white', rect)
 
 
 def main():
@@ -95,6 +145,8 @@ def main():
     turn = 0
     while run:
         clock.tick(FPS)
+        if black_score == 12 or white_score == 12:
+            run = False
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
