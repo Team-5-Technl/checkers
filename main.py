f7ba5f48888f94790571fc4452ba263b64842a45
lakshmichedella <lchedella_s@nlesd.ca>
2022-08-24 09:52:18 -0230

    Update main.py
    New code allows double capturing. Small bug: if you select a piece and press end turn, you will receive ValueError as selected piece is not in opposite dict.

--- main.py
+++ main.py
@@ -9,28 +9,29 @@ WIDTH, HEIGHT = 1200, 800
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 BOARD = pygame.image.load(os.path.join('Assets', 'Board.png'))
 FPS = 10
-black_color = (88, 16, 0)
-selected_black_color = (51, 41, 39)
+selected_black_color = (88, 16, 0)
 white_score = 0
-black_score = 0
+red_score = 0
 FONT = pygame.font.SysFont('Consolas', 40)
 ####################
 white_pieces = {}
-black_pieces = {}
+red_pieces = {}
 kings = {}
 
+
 def draw_side_bar():
-    top_text = FONT.render((f"Checkers score:"), 1, (255, 255, 255))
+    top_text = FONT.render(f"Checkers score:", True, (255, 255, 255))
     WINDOW.blit(top_text, (840, 50))
     pygame.draw.ellipse(WINDOW, 'white', (900, 250, 50, 50))
     pygame.draw.ellipse(WINDOW, 'red', (900, 500, 50, 50))
     pygame.draw.rect(WINDOW, (100, 100, 100), pygame.Rect(1030, 250, 100, 50))
     pygame.draw.rect(WINDOW, (100, 100, 100), pygame.Rect(1030, 500, 100, 50))
-    white_score_text = FONT.render((f"    |  {white_score}"), 1, (255, 255, 255))
-    red_score_text = FONT.render((f"    |  {black_score}"), 1, (255, 255, 255))
+    white_score_text = FONT.render(f"    |  {white_score}", True, (255, 255, 255))
+    red_score_text = FONT.render(f"    |  {red_score}", True, (255, 255, 255))
     WINDOW.blit(white_score_text, (900, 255))
     WINDOW.blit(red_score_text, (900, 505))
     pygame.display.update()
+
 
 def draw_window():
     WINDOW.fill((100, 100, 100))
@@ -42,8 +43,14 @@ def draw_window():
             pygame.draw.ellipse(WINDOW, 'white', rectangle_i)
         else:
             rectangle_i = pygame.Rect(25 + (200 * (i % 4) + (100 * (i // 4 % 2))), 225 + 100 * (i // 4), 50, 50)
-            black_pieces['rectangle_' + str(i + 1)] = rectangle_i
-            pygame.draw.ellipse(WINDOW, black_color, rectangle_i)
+            red_pieces['rectangle_' + str(i + 1)] = rectangle_i
+            pygame.draw.ellipse(WINDOW, 'red', rectangle_i)
+
+
+def end_turn():
+    end_turn_text = FONT.render('END TURN', True, 'black')
+    pygame.draw.rect(WINDOW, 'white', pygame.Rect(900, 700, 200, 100), False)
+    WINDOW.blit(end_turn_text, (910, 740))
 
 
 def select_white_piece(x):
@@ -52,18 +59,19 @@ def select_white_piece(x):
     return True
 
 
-def select_black_piece(x):
-    rect = list(black_pieces.values())[list(black_pieces.values()).index(x)]
+def select_red_piece(x):
+    rect = list(red_pieces.values())[list(red_pieces.values()).index(x)]
     pygame.draw.ellipse(WINDOW, selected_black_color, rect)
     return True
 
 
 def move_white_piece(x):
+    global white_score
     pos = pygame.mouse.get_pos()
     rect = list(white_pieces.values())[list(white_pieces.values()).index(x)]
     if 50 < abs(pos[0]-rect.center[0]) < 150 and 50 < pos[1]-rect.center[1] < 150:
         new_place = (rect.x + ([-100, 100][pos[0] - rect.x > 0]), rect.y + 100, 50, 50)
-        if new_place not in (white_pieces | black_pieces).values():
+        if new_place not in (white_pieces | red_pieces).values() and new_place[0] < 800:
             '''
             This is for when we have kings
 if rect in kings.values():
@@ -76,9 +84,8 @@ if rect in kings.values():
             rect.y += 100
             pygame.draw.ellipse(WINDOW, 'white', rect)
             return True
-        if new_place not in white_pieces.values() and new_place in black_pieces.values():
-            capture_black_piece(x)
-            return True
+        if new_place not in white_pieces.values() and new_place in red_pieces.values():
+            capture_red_piece(x)
         else:
             print('Another piece at selected location. Try again')
             pygame.draw.ellipse(WINDOW, 'white', rect)
@@ -87,63 +94,71 @@ if rect in kings.values():
         pygame.draw.ellipse(WINDOW, 'white', rect)
 
 
-def move_black_piece(x):
+def move_red_piece(x):
+    global red_score
     pos = pygame.mouse.get_pos()
-    rect = list(black_pieces.values())[list(black_pieces.values()).index(x)]
+    print(pos)
+    rect = list(red_pieces.values())[list(red_pieces.values()).index(x)]
     if 50 < abs(pos[0]-rect.center[0]) < 150 and -150 < pos[1]-rect.center[1] < -50:
         new_place = (rect.x+([-100, 100][pos[0]-rect.x > 0]), rect.y-100, 50, 50)
-        if new_place not in (white_pieces | black_pieces).values():
+        if new_place not in (white_pieces | red_pieces).values() and new_place[0] < 800:
             pygame.draw.ellipse(WINDOW, 'black', rect)
             rect.x += ([-100, 100][pos[0]-rect.x > 0])
             rect.y -= 100
-            pygame.draw.ellipse(WINDOW, black_color, rect)
+            pygame.draw.ellipse(WINDOW, 'red', rect)
             return True
-        if new_place not in black_pieces.values() and new_place in white_pieces.values():
+        if new_place not in red_pieces.values() and new_place in white_pieces.values():
             capture_white_piece(x)
-            return True
         else:
             print('Another piece at selected location. Try again')
-            pygame.draw.ellipse(WINDOW, black_color, rect)
+            pygame.draw.ellipse(WINDOW, 'red', rect)
     else:
         print('Illegal move. Try again')
-        pygame.draw.ellipse(WINDOW, black_color, rect)
+        pygame.draw.ellipse(WINDOW, 'red', rect)
 
 
-# for black pieces
+# for red pieces
 def capture_white_piece(x):
-    global black_score
+    global red_score
+    end_turn()
     pos = pygame.mouse.get_pos()
-    rect = list(black_pieces.values())[list(black_pieces.values()).index(x)]
+    rect = list(red_pieces.values())[list(red_pieces.values()).index(x)]
     new_place = (rect.x + ([-100, 100][pos[0] - rect.x > 0]), rect.y - 100, 50, 50)
-    if (rect.x + ([-200, 200][pos[0] - rect.x > 0]), rect.y - 200, 50, 50) not in (white_pieces | black_pieces).values():
+    newer_place = (rect.x + ([-200, 200][pos[0] - rect.x > 0]), rect.y - 200, 50, 50)
+
+    if newer_place not in (white_pieces | red_pieces).values() and newer_place[1] > 0 and newer_place[0] < 800:
         pygame.draw.ellipse(WINDOW, 'black', rect)
         rect.x += ([-200, 200][pos[0] - rect.x > 0])
         rect.y -= 200
         white_capture = list(white_pieces.keys())[list(white_pieces.values()).index(new_place)]
-        pygame.draw.ellipse(WINDOW, black_color, rect)
+        pygame.draw.ellipse(WINDOW, 'red', rect)
         pygame.draw.ellipse(WINDOW, 'black', white_pieces[white_capture])
         del white_pieces[white_capture]
-        black_score += 1
+        red_score += 1
+        return True
     else:
         print('Capture failed. Another piece. Try again')
-        pygame.draw.ellipse(WINDOW, black_color, rect)
+        pygame.draw.ellipse(WINDOW, 'red', rect)
 
 
 # for white pieces
-def capture_black_piece(x):
+def capture_red_piece(x):
     global white_score
+    end_turn()
     pos = pygame.mouse.get_pos()
     rect = list(white_pieces.values())[list(white_pieces.values()).index(x)]
     new_place = (rect.x + ([-100, 100][pos[0] - rect.x > 0]), rect.y + 100, 50, 50)
-    if (rect.x + ([-200, 200][pos[0] - rect.x > 0]), rect.y + 200, 50, 50) not in (white_pieces | black_pieces).values():
+    newer_place = (rect.x+([-200, 200][pos[0] - rect.x > 0]), rect.y + 200, 50, 50)
+    if newer_place not in (white_pieces | red_pieces).values() and newer_place[0] < 800 and newer_place[1] < 800:
         pygame.draw.ellipse(WINDOW, 'black', rect)
         rect.x += ([-200, 200][pos[0] - rect.x > 0])
         rect.y += 200
-        black_capture = list(black_pieces.keys())[list(black_pieces.values()).index(new_place)]
+        red_capture = list(red_pieces.keys())[list(red_pieces.values()).index(new_place)]
         pygame.draw.ellipse(WINDOW, 'white', rect)
-        pygame.draw.ellipse(WINDOW, 'black', black_pieces[black_capture])
-        del black_pieces[black_capture]
+        pygame.draw.ellipse(WINDOW, 'black', red_pieces[red_capture])
+        del red_pieces[red_capture]
         white_score += 1
+        return True
     else:
         print('Capture failed. Another piece. Try again')
         pygame.draw.ellipse(WINDOW, 'white', rect)
@@ -159,7 +174,7 @@ def main():
     turn = 0
     while run:
         clock.tick(FPS)
-        if black_score == 12 or white_score == 12:
+        if red_score == 12 or white_score == 12:
             run = False
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -170,15 +185,19 @@ def main():
             if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 pos = pygame.mouse.get_pos()
                 mouse_click += 1
-                print(['white turn', 'black turn'][turn % 2])
+                print(['white turn', 'red turn'][turn % 2])
+                if 900 < pos[0] < 1100 and 700 < pos[1] < 800:
+                    turn += 1
+                    WINDOW.fill((100, 100, 100), (900, 700, 200, 100))
+                    continue
                 if selected is False:
-                    for i in [white_pieces.values(), black_pieces.values()][turn % 2]:
+                    for i in [white_pieces.values(), red_pieces.values()][turn % 2]:
                         # works. but object is a rectangle. it considers that whole are a piece, not just the circle
                         if abs(i.x + 25 - pos[0]) <= 25 and abs(i.y + 25 - pos[-1]) <= 25:
                             if turn % 2 == 0:
                                 j = select_white_piece(i)
                             else:
-                                j = select_black_piece(i)
+                                j = select_red_piece(i)
                             if j:
                                 select = i
                                 selected = True
@@ -187,7 +206,7 @@ def main():
                     if turn % 2 == 0:
                         v = move_white_piece(select)
                     else:
-                        v = move_black_piece(select)
+                        v = move_red_piece(select)
                     if v:
                         turn += 1
                     selected = False
